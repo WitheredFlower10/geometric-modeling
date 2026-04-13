f1d5a0d58d7fd6be370cfeda144baade37a5a856
Hakim Justine <Justine.hakim@edu.esiee.fr>
2026-04-13 11:50:22 +0200

    Initial commit

---
+++ myVertex.cpp
@@ -0,0 +1,18 @@
+#include "myVertex.h"
+#include "myFace.h"
+#include "myHalfedge.h"
+#include "myVector3D.h"
+
+
+myVertex::myVertex(void) {
+  point = NULL;
+  originof = NULL;
+  normal = new myVector3D(1.0, 1.0, 1.0);
+}
+
+myVertex::~myVertex(void) {
+  if (normal)
+    delete normal;
+}
+
+void myVertex::computeNormal() { /**** TODO ****/ }
